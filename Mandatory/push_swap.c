70bc4f55fac875f2c8c7e53b4bb518725cb06d11
Abdellatif Aghla <aaghla@student.1337.ma>
2024-02-07 23:44:29 +0100

    in progress

--- Mandatory/push_swap.c
+++ Mandatory/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: aaghla <aaghla@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/31 16:28:36 by aaghla            #+#    #+#             */
-/*   Updated: 2024/02/07 22:05:15 by aaghla           ###   ########.fr       */
+/*   Updated: 2024/02/07 23:43:45 by aaghla           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -73,9 +73,61 @@ void	set_target_b(t_list *a, t_list *b)
 	}
 }
 
+void	sort_arr(int **arr, int n)
+{
+	int	i;
+	int	j;
+	int	temp;
+
+	i = -1;
+	while (++i < n)
+	{
+		j = -1;
+		while (++j < n)
+		{
+			if ((*arr)[i] < (*arr)[j])
+			{
+				temp = (*arr)[i];
+				(*arr)[i] = (*arr)[j];
+				(*arr)[j] = temp;
+			}
+		}
+	}
+	// printf("here in sort\n");
+}
+
+int	get_med(t_list *stack)
+{
+	int	*arr;
+	int	med;
+	int	i;
+
+	i = 0;
+	med = ft_lstsize(stack);
+	// printf("here in get_med\n");
+	arr = (int *)malloc(ft_lstsize(stack) * sizeof(int));
+	while (stack)
+	{
+		arr[i++] = stack->num;
+		stack = stack->next;
+	}
+	i = 0;
+	// while (i < med)
+	// 	printf("| %d |\n", arr[i++]);
+	sort_arr(&arr, med);
+	// printf("AFTER Sort:");
+	// while (i < med)
+	// 	printf("| %d |\n", arr[i++]);
+	// printf("\n       _________\n");
+	med = arr[med / 2];
+	// printf("med = %d | med num = %d\n", med, arr[med]);
+	return (med);
+}
+
 void	sort_stack(t_list **a, t_list **b)
 {
 	int	count;
+	int	med;
 
 	count = ft_lstsize(*a);
 	if (count-- > 3)
@@ -84,11 +136,16 @@ void	sort_stack(t_list **a, t_list **b)
 		pb(a, b);
 	while (count-- > 3)
 	{
-		set_index(*a);
-		set_index(*b);
-		set_target_a(*a, *b);
-		cost_calc(*a, *b);
-		push_to_b(a, b);
+		med = get_med(*a);
+		if ((*a)->num > med)
+			pb(a, b);
+		else
+			ra(a);
+		// set_index(*a);
+		// set_index(*b);
+		// set_target_a(*a, *b);
+		// cost_calc(*a, *b);
+		// push_to_b(a, b);
 	}
 	three_sort(a);
 	while (*b)
@@ -96,7 +153,7 @@ void	sort_stack(t_list **a, t_list **b)
 		set_index(*a);
 		set_index(*b);
 		set_target_b(*a, *b);
-		cost_calc(*b, *a);
+		// cost_calc(*b, *a);
 		push_to_a(a, b);
 	}
 	bring_min_top(a);
